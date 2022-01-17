cc59b593a5fb8ebdf5e290469224ccc91733926f
skdfx <97214981+skdfx@users.noreply.github.com>
2022-01-17 19:56:50 +0530

    Add files via upload
    calculator using Node.js and express.js

---
+++ calculator.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-Parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+app.get("/", function(req,res){
+res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function(req,res){
+var num1 = Number(req.body.n1);
+var num2 = Number(req.body.n2);
+var result=num1+num2;
+
+
+  res.send("The result of the calculation is" + result);
+});
+app.listen(3000, function(){
+console.log("Server started on the port 3000");
+});
